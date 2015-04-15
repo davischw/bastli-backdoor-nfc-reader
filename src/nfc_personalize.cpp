d2ba68c269cf6a282729870833d4bf0d2fd81627
Dominik Böhi <dominik.boehi@gmail.com>
2015-04-15 20:23:42 +0200

    Use class for NfcContext

--- src/nfc_personalize.cpp
+++ src/nfc_personalize.cpp
@@ -3,31 +3,25 @@
 //Boost Logging
 #include<boost/log/trivial.hpp>
 
+#include<NfcContext.hpp>
+
 int main() {
 
   const size_t max_devices = 8;
-  nfc_connstring devices[max_devices];
 
   //Setup nfc context
-  nfc_context *context;
-  nfc_init(&context);
-
-  if (context == NULL) {
-    BOOST_LOG_TRIVIAL(fatal) << "Failed to initalize NFC context";
-    return 1;
-  }
+  NfcContext context;
 
   //Look for NFC readers
-  size_t device_count = nfc_list_devices(context, devices, max_devices);
+  
+  auto devices = context.list_devices(max_devices);
 
-  if (device_count <= 0) {
+  if (devices->empty()) {
     BOOST_LOG_TRIVIAL(error) << "No NFC devices found";
     return 1;
   } else {
-    BOOST_LOG_TRIVIAL(info) << "Found " << device_count << " devices";
+    BOOST_LOG_TRIVIAL(info) << "Found " << devices->size() << " devices";
   }
 
-  nfc_exit(context);
-
   return 0;
-}
+};
